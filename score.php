4cb02a4ef77d88c8430cd9723be1e218689bbe12
yumi.h <hyumi0184@gmail.com>
2025-03-02 00:31:28 +0900

    Add files via upload

---
+++ score.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>
+       <?php
+
+       //変数に値を代入する
+       $score1〜score10 = (80+60+55+40+100+25+80+95+30+60);
+
+       // /変数と数値を計算し、その結果を出力する
+       echo $score1〜score10 / 10;
+       
+       ?>
+   </p>
+        
+</body>
+
+</html>
